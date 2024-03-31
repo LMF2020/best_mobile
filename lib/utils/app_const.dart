909a341162e6ba6bc48d40f31ef9f0b199d84a07
Josh <jiangzx0526@gmail.com>
2024-03-31 23:32:41 +0800

    fix bug

--- lib/utils/app_const.dart
+++ lib/utils/app_const.dart
@@ -40,5 +40,5 @@ class APP {
   static const String keyJoinDisplayName = "joinDisplayName";
   static const String keyMeetingHistory = "meetingHistory";
   static const String clientVerAndroid = "v1.0.3";
-  static const String clientVerIOS = "v1.0.0";
+  static const String clientVerIOS = "v1.0.1";
 }
